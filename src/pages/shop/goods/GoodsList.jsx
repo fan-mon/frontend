777aad050812dbb7c05b4359629d5ba069bea239
ccc700 <700eeu@gmail.com>
2024-09-27 16:49:34 +0900

    [Modify] public에 도메인 폴더 추가

--- src/pages/shop/goods/GoodsList.jsx
+++ src/pages/shop/goods/GoodsList.jsx
@@ -1,10 +1,12 @@
 import "./communal.css";
 import "./list.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import GoodsNav from "./GoodsNav";
 
 function GoodsList(){
     return(
         <>
+            <GoodsNav/>
             <section class="goods-frame">
                 <div class="container">
                     <div class="section-header">
@@ -15,61 +17,73 @@ function GoodsList(){
                             <div class="col-md-3 col-sm-4">
                                 <div class="single-goods">
                                     <div class="single-goods-bg">
-                                        <img src="#" alt="single-goods images"/>
+                                        <img src={`${process.env.PUBLIC_URL}/shop/goods/logo_white.png`} alt="single-goods images"/>
                                         <div class="single-goods-bg-overlay"></div>
                                     </div>
                                     <h4>귀여운 응원봉</h4>
                                     <p class="goods-price">59,000원</p>
                                     <button class="add-to-cart">Add to Cart</button>
-                                    <a href="/goods/detail"><button class="more-info">More Info</button></a>
+                                    <a href="/shop/goods/detail"><button class="more-info">More Info</button></a>
                                 </div>
                             </div>
                             <div class="col-md-3 col-sm-4">
                                 <div class="single-goods">
                                     <div class="single-goods-bg">
-                                        <img src="#" alt="single-goods images"/>
+                                        <img src={`${process.env.PUBLIC_URL}/shop/goods/logo_white.png`} alt="single-goods images"/>
                                         <div class="single-goods-bg-overlay"></div>
                                     </div>
                                     <h4>귀여운 응원봉</h4>
                                     <p class="goods-price">59,000원</p>
                                     <button class="add-to-cart">Add to Cart</button>
-                                    <a href="/goods/detail"><button class="more-info">More Info</button></a>
+                                    <a href="/shop/goods/detail"><button class="more-info">More Info</button></a>
                                 </div>
                             </div>
                             <div class="col-md-3 col-sm-4">
                                 <div class="single-goods">
                                     <div class="single-goods-bg">
-                                        <img src="#" alt="single-goods images"/>
+                                        <img src={`${process.env.PUBLIC_URL}/shop/goods/logo_white.png`} alt="single-goods images"/>
                                         <div class="single-goods-bg-overlay"></div>
                                     </div>
                                     <h4>귀여운 응원봉</h4>
                                     <p class="goods-price">59,000원</p>
                                     <button class="add-to-cart">Add to Cart</button>
-                                    <a href="/goods/detail"><button class="more-info">More Info</button></a>
+                                    <a href="/shop/goods/detail"><button class="more-info">More Info</button></a>
                                 </div>
                             </div>
                             <div class="col-md-3 col-sm-4">
                                 <div class="single-goods">
                                     <div class="single-goods-bg">
-                                        <img src="#" alt="single-goods images"/>
+                                        <img src={`${process.env.PUBLIC_URL}/shop/goods/logo_white.png`} alt="single-goods images"/>
                                         <div class="single-goods-bg-overlay"></div>
                                     </div>
                                     <h4>귀여운 응원봉</h4>
                                     <p class="goods-price">59,000원</p>
                                     <button class="add-to-cart">Add to Cart</button>
-                                    <a href="/goods/detail"><button class="more-info">More Info</button></a>
+                                    <a href="/shop/goods/detail"><button class="more-info">More Info</button></a>
                                 </div>
                             </div>
                             <div class="col-md-3 col-sm-4">
                                 <div class="single-goods">
                                     <div class="single-goods-bg">
-                                        <img src="#" alt="single-goods images"/>
+                                        <img src={`${process.env.PUBLIC_URL}/shop/goods/logo_white.png`} alt="single-goods images"/>
                                         <div class="single-goods-bg-overlay"></div>
                                     </div>
                                     <h4>귀여운 응원봉</h4>
                                     <p class="goods-price">59,000원</p>
                                     <button class="add-to-cart">Add to Cart</button>
-                                    <a href="/goods/detail"><button class="more-info">More Info</button></a>
+                                    <a href="/shop/goods/detail"><button class="more-info">More Info</button></a>
+                                </div>
+                            </div>
+                            <div class="col-md-3 col-sm-4">
+                                <div class="single-goods">
+                                    <div class="single-goods-bg">
+                                        <img src={`${process.env.PUBLIC_URL}/shop/goods/logo_white.png`} alt="single-goods images"/>
+                                        <div class="single-goods-bg-overlay"></div>
+                                    </div>
+                                    <h4>귀여운 응원봉</h4>
+                                    <p class="goods-price">59,000원</p>
+                                    <button class="add-to-cart">Add to Cart</button>
+                                    <a href="/shop/goods/detail"><button class="more-info">More Info</button></a>
                                 </div>
                             </div>
                         </div>
