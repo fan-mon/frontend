a199b6f844d727751971dd53d99ac9388466b030
ccc700 <700eeu@gmail.com>
2024-09-27 15:50:28 +0900

    [bugfix] 다시만들었어요

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import ChatPage from "./pages/ChatPage";
+import GoodsList from "./goods/GoodsList";
+import GoodsDetail from './goods/GoodsDetail';
 
 
 function App() {
@@ -9,7 +11,8 @@ function App() {
       <Router>
         <Routes>
           <Route path="/chat" element={<ChatPage />} />
-
+          <Route path="/goods" element={<GoodsList/>}/>
+          <Route path="/goods/detail" element={<GoodsDetail/>}/>
         </Routes>
       </Router>
   );
---
+++ src/goods/GoodsDetail.js
@@ -0,0 +1,83 @@
+import "./communal.css";
+import "./list.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function GoodsList(){
+    return(
+        <>
+            <section class="goods-frame">
+                <div class="container">
+                    <div class="section-header">
+                        <h2>전체보기</h2>
+                    </div>
+                    <div class="goodslist-content">
+                        <div class="row">
+                            <div class="col-md-3 col-sm-4">
+                                <div class="single-goods">
+                                    <div class="single-goods-bg">
+                                        <img src="#" alt="single-goods images"/>
+                                        <div class="single-goods-bg-overlay"></div>
+                                    </div>
+                                    <h4>귀여운 응원봉</h4>
+                                    <p class="goods-price">59,000원</p>
+                                    <button class="add-to-cart">Add to Cart</button>
+                                    <a href="/goods/detail"><button class="more-info">More Info</button></a>
+                                </div>
+                            </div>
+                            <div class="col-md-3 col-sm-4">
+                                <div class="single-goods">
+                                    <div class="single-goods-bg">
+                                        <img src="#" alt="single-goods images"/>
+                                        <div class="single-goods-bg-overlay"></div>
+                                    </div>
+                                    <h4>귀여운 응원봉</h4>
+                                    <p class="goods-price">59,000원</p>
+                                    <button class="add-to-cart">Add to Cart</button>
+                                    <a href="/goods/detail"><button class="more-info">More Info</button></a>
+                                </div>
+                            </div>
+                            <div class="col-md-3 col-sm-4">
+                                <div class="single-goods">
+                                    <div class="single-goods-bg">
+                                        <img src="#" alt="single-goods images"/>
+                                        <div class="single-goods-bg-overlay"></div>
+                                    </div>
+                                    <h4>귀여운 응원봉</h4>
+                                    <p class="goods-price">59,000원</p>
+                                    <button class="add-to-cart">Add to Cart</button>
+                                    <a href="/goods/detail"><button class="more-info">More Info</button></a>
+                                </div>
+                            </div>
+                            <div class="col-md-3 col-sm-4">
+                                <div class="single-goods">
+                                    <div class="single-goods-bg">
+                                        <img src="#" alt="single-goods images"/>
+                                        <div class="single-goods-bg-overlay"></div>
+                                    </div>
+                                    <h4>귀여운 응원봉</h4>
+                                    <p class="goods-price">59,000원</p>
+                                    <button class="add-to-cart">Add to Cart</button>
+                                    <a href="/goods/detail"><button class="more-info">More Info</button></a>
+                                </div>
+                            </div>
+                            <div class="col-md-3 col-sm-4">
+                                <div class="single-goods">
+                                    <div class="single-goods-bg">
+                                        <img src="#" alt="single-goods images"/>
+                                        <div class="single-goods-bg-overlay"></div>
+                                    </div>
+                                    <h4>귀여운 응원봉</h4>
+                                    <p class="goods-price">59,000원</p>
+                                    <button class="add-to-cart">Add to Cart</button>
+                                    <a href="/goods/detail"><button class="more-info">More Info</button></a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </>
+    )
+};
+
+export default GoodsList;
