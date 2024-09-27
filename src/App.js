70b3c5e202de82e38ad6405c2550dccf0ca5ac46
ccc700 <700eeu@gmail.com>
2024-09-27 16:38:24 +0900

    [Modify] js에서 jsx로 변환2

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import './App.css';
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import ChatPage from "./pages/ChatPage";
-
+import GoodsList from './pages/shop/goods/GoodsList';
+import GoodsDetail from './pages/shop/goods/GoodsDetail';
 
 
 function App() {
@@ -10,7 +11,8 @@ function App() {
       <Router>
         <Routes>
           <Route path="/chat" element={<ChatPage />} />
-
+          <Route path="/shop/goods" element={<GoodsList/>}/>
+          <Route path="/shop/goods/detail" element={<GoodsDetail/>}/>
         </Routes>
       </Router>
   );
