0fead9ba13fb742fd2b1d3f9e80566c5b70cb1f5
uhyunjin <uhyeonjin0619@gmail.com>
2024-09-30 21:36:38 +0900

    [Feat] 아티스트 페이지 및 채팅 페이지 구현 - 아티스트 페이지 구독 체크 버튼 - 채팅 페이지 ui 조금 구현

--- src/App.js
+++ src/App.js
@@ -4,19 +4,24 @@ import ChatPage from "./pages/chat/ChatPage";
 import GoodsList from './pages/shop/goods/GoodsList';
 import GoodsDetail from './pages/shop/goods/GoodsDetail';
 import Header from './pages/common/Header'
+import MessageBox from "./pages/chat/MessageBox";
+import ArtistPage from "./pages/Artist/ArtistPage";
 
 
 function App() {
     // 유저 하드코딩
     // 세션 유지 이후 변경 예정
     const user='summer'
+    const artistuuid='1234'
 
   return (
       <BrowserRouter>
         <Header />
         <div className="content">
             <Routes>
-              <Route path="/chat" element={<ChatPage user={user}/>} />
+              <Route path="/chat/ws" element={<ChatPage user={user}/>} />
+              <Route path="/chat/subscribe" element={<ArtistPage artistuuid={artistuuid}/>} />
+              <Route path="/chat/box" element={<MessageBox />} />
               <Route path="/shop/goods" element={<GoodsList/>}/>
               <Route path="/shop/goods/detail" element={<GoodsDetail/>}/>
             </Routes>
