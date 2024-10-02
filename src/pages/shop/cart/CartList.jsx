1fb0d466e62e783186c87320c03c6c55a1579b6b
ccc700 <700eeu@gmail.com>
2024-10-02 15:34:51 +0900

    [Modify] 장바구니 리스트 수량 변경 기능 추가

--- src/pages/shop/cart/CartList.jsx
+++ src/pages/shop/cart/CartList.jsx
@@ -5,14 +5,24 @@ import * as Icon from 'react-bootstrap-icons';
 
 
 function CartList(){
-    const products = [
+
+    const initialProducts = [
         { id: 1, name: "귀여운 요술봉", price: 59000, quantity: 1 },
-        { id: 2, name: "안귀여운 요술봉", price: 39000, quantity: 2 } // 하드코딩된 수량
+        { id: 2, name: "안귀여운 요술봉", price: 39000, quantity: 2 } 
     ];
+
+    const [products, setProducts] = useState(initialProducts);
 
     // 총 수량과 총 가격 계산
     const totalQuantity = products.reduce((total, product) => total + parseInt(product.quantity), 0);
     const totalPrice = products.reduce((total, product) => total + (product.price * product.quantity), 0).toLocaleString();
+
+    // 수량 변경 핸들러 함수
+    const handleQuantityChange = (id, newQuantity) => {
+        setProducts(products.map(product => 
+            product.id === id ? { ...product, quantity: newQuantity } : product
+        ));
+    };
 
     return(
         <>
@@ -21,12 +31,18 @@ function CartList(){
                     <h2>장바구니</h2>
                     <table class="cart-content-list">
                         {products.map((product, index) => (
-                            <tr>
-                                <td class="cart-list-no cart-list-center">1</td>
+                            <tr key={product.id}>
+                                <td class="cart-list-no cart-list-center">{index + 1}</td>
                                 <td class="cart-list-file cart-list-center"><a href="/shop/goods/detail"><img src={`${process.env.PUBLIC_URL}/shop/common/monster1.png`} alt="cart list image"/></a></td>
-                                <td class="cart-list-name"><a href="/shop/goods/detail">귀여운 요술봉</a></td>
+                                <td class="cart-list-name"><a href="/shop/goods/detail">{product.name}</a></td>
                                 <td class="cart-list-qty cart-list-center">
-                                    <input type="number" name={`goods-qty-${product.id}`} min="1" max="10" value="1"/>
+                                    <input 
+                                        type="number" 
+                                        name={`goods-qty-${product.id}`} 
+                                        min="1" 
+                                        value={product.quantity} 
+                                        onChange={(e) => handleQuantityChange(product.id, parseInt(e.target.value))}
+                                    />
                                 </td>
                                 <td class="cart-list-price">{(product.price * product.quantity).toLocaleString()}원</td>
                                 <td class="cart-list-delete cart-list-center"><Icon.XLg class="delete-icon" onClick="#" /></td>
