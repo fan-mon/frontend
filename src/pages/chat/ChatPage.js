a732305f17daae113f37915ce9e2064b6e605180
uhyunjin <uhyeonjin0619@gmail.com>
2024-10-08 11:58:35 +0900

    [Feat] 채팅창 CSS - 파일전송 작업중 - 유저 밴 작업중

--- src/pages/chat/ChatPage.js
+++ src/pages/chat/ChatPage.js
@@ -3,6 +3,7 @@ import SockJS from 'sockjs-client';
 import { Stomp } from '@stomp/stompjs';
 import {useParams} from "react-router-dom";
 import ChatRoom from "./ChatRoom";
+import axios from "axios";
 
 const ChatPage = ({ chatUuid }) => {
     const [stompClient, setStompClient] = useState(null);
@@ -16,23 +17,33 @@ const ChatPage = ({ chatUuid }) => {
     let useruuid = "";
     let destination="";
     if (role==='ARTIST'){
-        destination=`/pub/${artistUuid}/toFans`
         useruuid=null;
+        destination=`/pub/${artistUuid}/toFans`
     }else if(role==='USER'){
-        destination`/pub/${artistUuid}/${sender}/toArtist`
         useruuid=localStorage.getItem("uuid");
+        destination=`/pub/${artistUuid}/${useruuid}/toArtist`
     }
 
     useEffect(() => {
+        // 처음 로딩시 과거 메세지들을 가져온다
+        const fetchMessages = async ()=>{
+            try{
+                const response = await axios.get(`http://localhost:8080/chat/messages/${chatUuid}`);
+                setMessages(response.data)
+            }catch (e){
+                console.log(e)
+            }
+        }
+        fetchMessages();
         const socket = new SockJS(`http://localhost:8080/chat/ws`); // WebSocket 연결
         const client = Stomp.over(()=>socket); // Stomp 클라이언트 생성
 
         client.connect({}, (frame) => {
             console.log('Connected: ' + frame);
-
             // 유저와 아티스트의 구독 처리
             // 팬의 메세지는 아티스트와 발행한 팬만 구독
             client.subscribe(`/sub/${artistUuid}/fromFans`, (message) => {
+                console.log("receive message : "+message.body)
                 const parsedMsg=JSON.parse(message.body);
                 if (role==='ARTIST'||parsedMsg.user.useruuid===localStorage.getItem("uuid")){
                     setMessages(prevMessages => [...prevMessages, JSON.parse(message.body)]);
@@ -40,6 +51,7 @@ const ChatPage = ({ chatUuid }) => {
             });
             // 아티스트가 보낸 메세지는 아티스트와 유저 둘 다 구독
             client.subscribe(`/sub/${artistUuid}/toFans`, (message) => {
+                console.log("receive message : "+message.body)
                 setMessages(prevMessages => [...prevMessages, JSON.parse(message.body)]);
             });
 
@@ -54,37 +66,73 @@ const ChatPage = ({ chatUuid }) => {
             console.error('Broker reported error: ' + frame.headers['message']);
             console.error('Additional details: ' + frame.body);
         };
-
         client.activate();
-
-
         return () => {
             if (client) {
                 client.disconnect();
             }
         };
-    }, [artistUuid, sender, role]); // 의존성 배열에 artistUuid와 useruuid 추가
+    }, [artistUuid, useruuid, role]); // 의존성 배열에 artistUuid와 useruuid 추가
+
+    // 이미지 전송 함수
+    const sendImage = (image, filename) => {
+        if (stompClient && stompClient.connected) {
+            const imageData = {
+                artistmessageuuid:null,
+                messagetext: filename+"/"+image, // Base64 이미지 데이터
+                timestamp:null,
+                artist: {
+                    artistuuid: artistUuid,
+                },
+                chat: {
+                    chatuuid: chatUuid,
+                },
+            };
+            console.log(imageData);
+            stompClient.send("/pub/sendImage", {}, JSON.stringify(imageData));
+        } else {
+            console.error("STOMP client is not connected");
+        }
+    };
+
+    
 
     // 메세지 전송 함수
     const sendMessage = (message) => {
         if (stompClient && stompClient.connected) {
-            const messageData = {
-                messageuuid:null,
-                messagetext: message,
-                timestamp: null,
-                artist: {
-                    artistuuid: artistUuid,  // 아티스트 UUID
-                },
-                user: {
-                    useruuid: useruuid,  // 유저 UUID
-                },
-                messageFrom: role,
-                chat: {
-                    chatuuid: chatUuid,  // 채팅 UUID
-                },
-            };
-            console.log("messageData : "+JSON.stringify(messageData))
-            stompClient.send(destination, {}, JSON.stringify(messageData));
+            let messageData;
+            if (role==='USER'){
+                messageData = {
+                    usermessageuuid:null,
+                    messagetext: message,
+                    timestamp: null,
+                    artist: {
+                        artistuuid: artistUuid,  // 아티스트 UUID
+                    },
+                    user: {
+                        useruuid: useruuid,  // 유저 UUID
+                    },
+                    chat: {
+                        chatuuid: chatUuid,  // 채팅 UUID
+                    },
+                };
+                console.log("messageData : "+JSON.stringify(messageData))
+                stompClient.send(destination, {}, JSON.stringify(messageData));
+            }else {
+                messageData = {
+                    artistmessageuuid:null,
+                    messagetext: message,
+                    timestamp: null,
+                    artist: {
+                        artistuuid: artistUuid,  // 아티스트 UUID
+                    },
+                    chat: {
+                        chatuuid: chatUuid,  // 채팅 UUID
+                    },
+                };
+                console.log("messageData : "+JSON.stringify(messageData))
+                stompClient.send(destination, {}, JSON.stringify(messageData));
+            }
         } else {
             console.error("STOMP client is not connected");
         }
@@ -95,6 +143,7 @@ const ChatPage = ({ chatUuid }) => {
             <ChatRoom role={role}
                       messages={messages}
                       sendMessage={sendMessage}
+                      sendImage={sendImage}
             />
         </div>
     );
