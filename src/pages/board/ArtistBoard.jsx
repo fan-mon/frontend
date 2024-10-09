2ac1677b5ffd561a42735761bff6e1473b073cfd
uhyunjin <uhyeonjin0619@gmail.com>
2024-10-09 21:33:30 +0900

    [Feat] 아티스트 게시글 작성 칸 구현

--- src/pages/board/ArtistBoard.jsx
+++ src/pages/board/ArtistBoard.jsx
@@ -2,43 +2,54 @@ import {href} from "sockjs-client/lib/location";
 
 const ArtistBoard = ({ artistBoard }) => {
     return (
+
         <div className="artist-board-wrap">
             <div className="board-title">
                 Artist Board
             </div>
+            <div className="new-post-wrap">
+                <textarea className="writing-box" name="" id="" cols="30" maxLength="150" rows="10"
+                          placeholder="150자 이내 작성">
+                </textarea>
+                <div className="send-post">
+                    post
+                </div>
+            </div>
             {artistBoard && artistBoard.length > 0 ? (
                 artistBoard.map((board) => (
-                    <div key={board.artistboarduuid} className="board-content">
-                        <div className="content-left">
-                            <div className="writer-photo">
-                                <img src="" alt="wrtier-photo"/>
+                    <div>
+                        <div key={board.artistboarduuid} className="board-content">
+                            <div className="content-left">
+                                <div className="writer-photo">
+                                    <img src="" alt="wrtier-photo"/>
+                                </div>
+                                <div className="like">
+                                    <p>♥ {board.likecount}</p>
+                                </div>
                             </div>
-                            <div className="like">
-                                <p>💕 {board.likecount}</p>
+                            <div className="content-right">
+                                <div className="content-right-top">
+                                    <div className="wrtier">
+                                        {board.artist.name}
+                                    </div>
+                                    <div className="date">
+                                        {new Date(board.createdat).toLocaleDateString('ko-KR')} {new Date(board.createdat).toLocaleTimeString('ko-KR', {
+                                        hour: '2-digit',
+                                        minute: '2-digit'
+                                    })}
+                                    </div>
+                                </div>
+                                <div className="content-right-bottom">
+                                    <div className="content">
+                                        <p>Content: {board.content}</p>
+                                    </div>
+                                    <div className="content-img">
+                                        <img src="" alt="content-image"/>
+                                    </div>
+                                </div>
                             </div>
                         </div>
-                        <div className="content-right">
-                            <div className="content-right-top">
-                                <div className="wrtier">
-                                    {board.artist.name}
-                                </div>
-                                <div className="date">
-                                    {new Date(board.createdat).toLocaleDateString('ko-KR')} {new Date(board.createdat).toLocaleTimeString('ko-KR', {
-                                    hour: '2-digit',
-                                    minute: '2-digit'
-                                })}
-                                </div>
-                            </div>
-                            <div className="content-right-bottom">
-                                <div className="content">
-                                    <p>Content: {board.content}</p>
-                                </div>
-                                <div className="content-img">
-                                    <img src="" alt="content-image"/>
-                                </div>
-                            </div>
-                            <hr/>
-                        </div>
+                        <hr/>
                     </div>
                 ))
 
