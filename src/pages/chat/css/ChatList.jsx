21ffa5330d2cb26a37356a48b955655f82394d61
uhyunjin <uhyeonjin0619@gmail.com>
2024-10-12 17:39:30 +0900

    [Feat] 채팅 리스트 추가 - 헤더에 hover 시 채팅리스트 출력 - 리스트 별 채팅방 연결

--- src/pages/chat/css/ChatList.jsx
+++ src/pages/chat/css/ChatList.jsx
@@ -1,9 +1,33 @@
-const ChatList=()=>{
+import axios from "axios";
+import {useEffect, useState} from "react";
+import {useNavigate} from "react-router-dom";
+
+const ChatList=({chatList=[]})=>{
+
+    const navigate = useNavigate(); // useNavigate 훅을 사용하여 navigate 함수 얻기
+
+    const handleChatClick = (chatuuid) => {
+        navigate(`/chat/ws/${chatuuid}`); // URL로 이동
+    };
+
     return (
         <>
-            채팅 리스트 페이지입니다.
+            {chatList.length > 0 ?
+                (chatList.map(data => (
+                        <div key={data.chat.chatuuid} // 유일한 key 추가
+                             onClick={() => handleChatClick(data.chat.chatuuid)}
+                            className="chat-room-list">
+                            <div className="profile-photo"></div>
+                            < div className="artist-name">
+                                {data.chat.artist.name}
+                            </div>
+                        </div>
+                    )))
+                :
+                (<div  className="chat-room-list">새로운 채팅을 시작해보세요!</div>)
+            }
         </>
-    )
+    );
 }
 
 export default ChatList;
