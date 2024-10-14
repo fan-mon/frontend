ede83e538d251818d70cacf11e7734748a1dd62b
uhyunjin <uhyeonjin0619@gmail.com>
2024-10-14 20:11:56 +0900

    [Modify] 프로필 모달창 이동 버튼에 데이터 추가

--- src/pages/chat/chatAPI/subscription.js
+++ src/pages/chat/chatAPI/subscription.js
@@ -2,7 +2,7 @@ import axios from "axios";
 const SUBSCRIPTION_API_URL=`${process.env.REACT_APP_BACKEND_API_URL}/chat`
 const getList=async (useruuid, setChatList)=>{
     try{
-        const response = await axios.get(`${SUBSCRIPTION_API_URL}/subcription/${useruuid}`);
+        const response = await axios.get(`${SUBSCRIPTION_API_URL}/subscription/${useruuid}`);
         if (response.data===204){
             console.log("구독중인 아티스트 없음")
         }else {
