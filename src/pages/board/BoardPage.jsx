9e081456d525a98ae9f4a3191cb3d107d38b50f6
uhyunjin <uhyeonjin0619@gmail.com>
2024-10-14 19:05:33 +0900

    [Modify] css수정

--- src/pages/board/BoardPage.jsx
+++ src/pages/board/BoardPage.jsx
@@ -12,18 +12,16 @@ const BoardPage = () => {
     const {teamuuid} = useParams();
 
     useEffect(() => {
-        fetchUserInfo(); // 컴포넌트가 마운트될 때 사용자 정보 가져오기
-    },[]);
-
-    useEffect(() => {
         console.log(`username : ${username}`);
     }, [username]); // username이 업데이트될 때마다 로그 찍기
+
     const fetchUserInfo = async () => {
-        if (sessionStorage.getItem("role")==='USER'){
+        if (localStorage.getItem("role")==='USER'){
             try {
                 const response = await api.get('/users/myprofile');
                 setUsername(response.data.name); // 사용자 이름 상태 업데이트
                 localStorage.setItem("uuid", response.data.useruuid)
+                localStorage.setItem("status", response.data.status);
                 console.log(`username : ${username}`)
             } catch (error) {
                 console.error("사용자 정보 가져오기 오류:", error);
@@ -31,30 +29,37 @@ const BoardPage = () => {
         }
     };
 
+    useEffect( () => {
+        fetchUserInfo(); // 컴포넌트가 마운트될 때 사용자 정보 가져오기
+    },[]);
+
     return (
+        <body>
         <div className="whole-page">
             <div className="team-profile">
                 <TeamProfile teamuuid={teamuuid}/>
             </div>
             <div className="page">
-                    <div className="left-column">
-                        <ArtistBoard
+                <div className="left-column">
+                    <ArtistBoard
+                        teamUuid={teamuuid}/>
+                </div>
+                <div className="right-column">
+                    <div className="notice">
+                        <BoardNotice
                             teamUuid={teamuuid}/>
                     </div>
-                    <div className="right-column">
-                        <div className="notice">
-                            <BoardNotice
-                                teamUuid={teamuuid}/>
-                        </div>
-                        <hr/>
-                        <div className="fan-board">
-                            <FanBoard
-                                teamUuid={teamuuid}/>
-                        </div>
+                    <hr/>
+                    <div className="fan-board">
+                        <FanBoard
+                            teamUuid={teamuuid}/>
                     </div>
+                </div>
             </div>
         </div>
+        </body>
     );
+
 };
 
 export default BoardPage;
